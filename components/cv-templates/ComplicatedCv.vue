bbb583cac41cf9f84650ebbbf7110026347c99f8
Marian Hasiak <mancoppor@gmail.com>
2025-04-16 17:48:20 +0200

    Create test cv templates

---
+++ components/cv-templates/ComplicatedCv.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="flex flex-row w-full text-xs">
+    <div class="basis-1/3 bg-sky-900 text-white flex flex-col items-center">
+      <div class="image w-full aspect-square flex justify-center items-center">
+        <div class="aspect-square w-2/3 bg-sky-950 border-4 border-white rounded-full" />
+      </div>
+
+      <div class="flex flex-col items-center gap-6 w-full p-8 pt-0">
+        <div class="contact-section w-full">
+          <h2 class="font-black tracking-widest pb-2 mb-1 border-b-1 border-white">CONTACT</h2>
+          <p>Lorem ipsum dolor</p>
+          <p>Lorem ipsum dolor</p>
+          <p>Lorem ipsum dolor</p>
+          <p>Lorem ipsum dolor</p>
+          <p>Lorem ipsum dolor</p>
+        </div>
+
+        <div class="languages-section w-full">
+          <h2 class="font-black tracking-widest pb-2 mb-1 border-b-1 border-white">LANGUAGES</h2>
+          <p>Lorem ipsum dolor</p>
+          <p>Lorem ipsum dolor</p>
+          <p>Lorem ipsum dolor</p>
+          <p>Lorem ipsum dolor</p>
+          <p>Lorem ipsum dolor</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="basis-2/3 text-gray-700 p-8">
+      <div class="title-section mb-8">
+        <h1 class="text-4xl font-black">NAME SURNAME</h1>
+        <p class="text-xl font-light">FULL-STACK DEVELOPER</p>
+      </div>
+
+      <div class="brief-section mb-8">
+        <h2 class="font-bold text-xl tracking-widest pb-1 mb-2 border-b-1 border-gray-700">PROFILE</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+          explicabo
+          cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+      </div>
+
+      <div class="education-section">
+        <h2 class="font-bold text-xl tracking-widest pb-1 mb-2 border-b-1 border-gray-700">EDUCATION</h2>
+
+        <div>
+          <h3 class="font-bold text-base">University of Blahblah - Informatics</h3>
+          <p class="font-bold mb-2">Oct 2021 - Present</p>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+          explicabo
+          cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?
+        </p>        
+        
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+          explicabo
+          cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?
+        </p>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
---
+++ components/cv-templates/SimpleCv.vue
@@ -0,0 +1,110 @@
+<template>
+  <div>
+    <h1 class="text-2xl backface-hidden translate-z-0">Test</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h1 class="text-2xl">Test</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h1 class="text-2xl">Test</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h1 class="text-2xl">Test</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h1 class="text-2xl">Test</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+    <h3 class="text-xl">Mini test</h3>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
+      explicabo
+      cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style></style>
