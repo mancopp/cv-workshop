cdb5ee6fde805398a56832b8df59ba54a2c4e760
Marian Hasiak <mancoppor@gmail.com>
2025-04-16 17:53:28 +0200

    Move back to simple html file preview element

--- components/HtmlFilePreview.vue
+++ components/HtmlFilePreview.vue
@@ -1,131 +1,79 @@
 <template>
-  <div ref="zoomPinchEl" class="projection-wrapper flex flex-col justify-center items-center w-full h-screen p-5">
-
-    <zoompinch ref="zoompinchRef" v-model:transform="transform" :width="446" :height="3922" :offset="offset"
-      :min-scale="5" :max-scale="10" :rotation="false" :bounds="false" :mouse="false" :touch="false" :wheel="true"
-      :gesture="false">
-      <template #canvas>
-        <div class="w-[446px] bg-white text-black px-10 py-25 shadow-lg border-[1px]">
-          <h1 class="text-2xl">Test</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h1 class="text-2xl">Test</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h1 class="text-2xl">Test</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h1 class="text-2xl">Test</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h1 class="text-2xl">Test</h1>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-          <h3 class="text-xl">Mini test</h3>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat, sunt eos fugit ducimus pariatur
-            explicabo
-            cum maxime nulla ut illum vel dolorum libero unde. Odit molestias recusandae velit impedit non?</p>
-        </div>
-      </template>
-    </zoompinch>
+  <div ref="zoomEl" class="flex flex-col justify-center items-center w-full h-screen p-5">
+    <div ref="cvEl"
+      class="w-[700px] bg-white text-black shadow-lg border-[1px]">
+      <ComplicatedCv />
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { Zoompinch } from 'zoompinch';
-import 'zoompinch/style.css';
-
-const zoompinchRef = ref<InstanceType<typeof Zoompinch>>();
-const offset = reactive({ top: 10, right: 10, bottom: 10, left: 10 });
-const transform = ref({ x: 0, y: 0, scale: 1, rotate: 0 });
+import { jsPDF } from "jspdf";
+import { writeFile, readFile, BaseDirectory } from '@tauri-apps/plugin-fs';
+import ZoomShortcutHandler from '@/lib/zoom-shortcuts';
 
 
-function fit() {
-  transform.value = { x: 0, y: 0, scale: 1, rotate: 0 };
-}
+const zoomEl = ref<HTMLElement | null>(null);
+const cvEl = ref<HTMLElement | null>(null);
+
+const handleGPress = async (event) => {
+  if (event.key === 'g') {
+    console.log('Button clicked!');
+
+    const pdf = new jsPDF('p', 'px', 'a4');
+    pdf.html(cvEl.value as HTMLElement, {
+      callback: function (pdf) {
+        const pdfData = new Uint8Array(pdf.output('arraybuffer'));
+        writeFile('yourFileName1.pdf', pdfData, {
+          baseDir: BaseDirectory.Download,
+        });
+        console.log('PDF generated!');
+      },
+      // margin: [20, 20, 20, 20], // Set appropriate margins
+      autoPaging: 'text', // Crucial for handling text flow across pages
+      html2canvas: {
+        allowTaint: true,
+        letterRendering: true,
+        logging: false,
+        scale: 0.638, // Adjust the scale to fit content
+      }
+    });
+  }
+};
 
 onMounted(() => {
-  setTimeout(() => fit());
+  if (zoomEl.value) {
+    const zoomHandler = new ZoomShortcutHandler(zoomEl.value, {
+      initialZoom: 2,
+      minZoom: 1,
+      maxZoom: 5,
+      zoomStep: 0.5,
+    });
+    zoomHandler.register();
+  }
+
+  // window.addEventListener('keydown', async (event) => {
+  //   if (event.key === 'i') {
+  //     console.log('Button clicked!');
+
+  //     const input = await readFile('testcv.html', {
+  //       baseDir: BaseDirectory.Download,
+  //     });
+
+  //     const decoder = new TextDecoder();
+  //     const htmlContent = decoder.decode(input);
+
+  //     if (!cvEl.value) return;
+
+  //     cvEl.value.innerHTML = htmlContent;
+  //   }
+  // });
+
+  window.addEventListener('keydown', handleGPress);
 });
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleGPress);
+});
+
 </script>
